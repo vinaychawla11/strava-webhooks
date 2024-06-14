dbac724559f440cb568b16805a796063f510e8a9
Vinay Chawla <vinay.chawla11@gmail.com>
2024-06-14 17:55:17 +0530

    fix path to functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const serverless = require('serverless-http');
-const app = require('../src/api/index'); // Adjust the path to your Express app
+const app = require('../src/api/index.js'); // Adjust the path to your Express app
 
 module.exports.handler = serverless(app);
