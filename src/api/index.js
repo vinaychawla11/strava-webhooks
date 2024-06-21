d3cb66324be84c03194bbbd4edded0a40c2cc467
Vinay Chawla <vinay.chawla11@gmail.com>
2024-06-21 15:01:50 +0530

    fix path

--- src/api/index.js
+++ src/api/index.js
@@ -27,7 +27,7 @@ app.use(express.static('public'));
 
 // Serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
