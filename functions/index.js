c89f221ba4ed8f331be7d391075087b578bcef79
Vinay Chawla <vinay.chawla11@gmail.com>
2024-06-21 16:21:59 +0530

    cleanup remnants of vercel, heroku and netlify

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const serverless = require('serverless-http');
-const app = require('../src/api/index.js'); // Adjust the path to your Express app
-
-module.exports.handler = serverless(app);
