40dd6ae2211838f0809199a72992b3c652945f50
Vinay Chawla <vinay.chawla11@gmail.com>
2024-06-21 15:26:12 +0530

    Restructure and make a script.js instead of inline js in index.html

---
+++ src/public/script.js
@@ -0,0 +1,62 @@
+
+
+    const lightButton = document.getElementById("light");
+    const darkButton = document.getElementById("dark");
+    const solarButton = document.getElementById("solar");
+    const body = document.body;
+
+
+    // Apply cached item on reload
+    const theme = localStorage.getItem('theme');
+    const isSolar = localStorage.getItem('isSolar');
+
+    if (theme) {
+        body.classList.add(theme);
+        isSolar && body.classList.add('solar');
+    }
+
+    darkButton.onclick = () => {
+        body.classList.replace('light', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+
+    lightButton.onclick = () => {
+        body.classList.replace('dark', 'light');
+        localStorage.setItem('theme', 'light');
+
+    }
+
+    solarButton.onclick = () => {
+        if (body.classList.contains('solar')) {
+            body.classList.remove('solar')
+            solarButton.style.cssText = `
+    --bg-solar: var(--yellow);
+    `
+            solarButton.innerText = 'solarize';
+            localStorage.remove('isSolar');
+        }
+        else {
+            solarButton.style.cssText = `
+--bg-solar: white;
+`
+            body.classList.add('solar');
+            solarButton.innerText = 'normalize';
+            localStorage.setItem('isSolar', true);
+
+        }
+
+    }
+    const authorizeLink = document.getElementById('main-content');
+
+    themeLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.body.className = event.target.id;
+        });
+    });
+
+   
+    authorizeLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        mainContent.scrollIntoView({ behavior: 'smooth' });
+    });
